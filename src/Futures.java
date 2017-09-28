d76ef068ace2ee2fe63dc5dcb0a16a83f9b2777b
Sai Chandramouli <schand31@asu.edu>
2017-09-28 13:06:08 -0700

    with three threads, futures does it 1/3 time
    Is it linear?

--- src/Futures.java
+++ src/Futures.java
@@ -21,30 +21,42 @@ public class Futures {
         int amount = 0;
 
         // Futures ftw
-        int numThreads = 2;
+        int numThreads = 3;
         ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
         List<Future<Integer>> futureList = new ArrayList<>();
 
-        // Start thread for the first half of the numbers
+        // Start thread for the first third of the numbers
         FutureTask<Integer> futureTask_1 = new FutureTask<Integer>(new Callable<Integer>() {
             @Override
             public Integer call() {
-                return Futures.amountOfDivisibleBy(first, last/2, divisor);
+                System.out.println(String.format("From %s to %s", first, last/3));
+                return Futures.amountOfDivisibleBy(first, last/3, divisor);
             }
         });
         futureList.add(futureTask_1);
         executorService.execute(futureTask_1);
 
-        // Start thread for the second half of the numbers
+        // Start thread for the second third of the numbers
         FutureTask<Integer> futureTask_2 = new FutureTask<Integer>(new Callable<Integer>() {
             @Override
             public Integer call() {
-                return Futures.amountOfDivisibleBy(last / 2 + 1, last, divisor);
+                System.out.println(String.format("From %s to %s", last / 3 + 1, last/3 * 2));
+                return Futures.amountOfDivisibleBy(last / 3 + 1, last/3 * 2, divisor);
             }
         });
         futureList.add(futureTask_2);
         executorService.execute(futureTask_2);
 
+        // Start thread for the last third of the numbers
+        FutureTask<Integer> futureTask_3 = new FutureTask<Integer>(new Callable<Integer>() {
+            @Override
+            public Integer call() {
+                System.out.println(String.format("From %s to %s", last/3 * 2, last));
+                return Futures.amountOfDivisibleBy(last/3 * 2 + 1, last, divisor);
+            }
+        });
+        futureList.add(futureTask_3);
+        executorService.execute(futureTask_3);
 
         for (int j = 0; j < numThreads; j++) {
             Future<Integer> futureTask = futureList.get(j);
